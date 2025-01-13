576b55efb4412155d5979664abd3c620ed13bf5d
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-01-13 12:38:10 +0600

    structure

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors"
+import mongoose from "mongoose";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp"
+import cookieParser from "cookie-parser"
+import router from ".//routes/api.js";
+import {MONGODB_CONNECTION, PORT, MAX_JSON_SIZE, URL_ENCODING, WEB_CACHE, REQUEST_LIMIT_NUMBER, REQUEST_LIMIT_TIME} from "../server/src/config/config.js"
+
+const app = express();
+
+app.use(cors())
+app.use(express.json({limit:MAX_JSON_SIZE}))
+app.use(express.urlencoded({extended:URL_ENCODING}))
+app.use(hpp())
+app.use(helmet())
+app.use(cookieParser())
+
+// Rate limiter
+const limiter = rateLimit({windowMs:REQUEST_LIMIT_TIME, max:REQUEST_LIMIT_NUMBER})
+app.use(limiter)
+// web cache
+app.set('etag', WEB_CACHE)
+// MongoDB connection
+mongoose.connect(MONGODB_CONNECTION, {autoIndex: true}).then(()=>{
+    console.log("connected to MongoDB")
+}).catch(err=>{
+    console.log(err)
+})
+// set Api
+app.use("/api", router)
+
+// run app
+app.listen(PORT || 3030, ()=>{
+    console.log(`app running on port ${PORT}`)
+})
---
+++ server/src/config/config.js
@@ -0,0 +1,21 @@
+
+export const MONGODB_CONNECTION = "mongodb://localhost:27017/full-stack-project"; // Connection String is from MongoDB Atlas Cluster.
+export const JWT_SECRET =
+    "e5c843c644b2beaf141e4ed0cdae3d4cf2092f68a5d999335e5ec8d1b1cfddb4"; // JWT Secret string is generated from JWT secret generator . it will use to encode or decode JWT Token.
+export const JWT_EXPIRATION_DURATION = 60 * 60 * 24; // It will use to encode or decode JWT Token.
+
+// export const EMAIL_HOST = "smtp.gmail.com"
+// export const EMAIL_PORT = 465
+// export const EMAIL_USER = "jahirulislamjantu@gmail.com"
+// export const EMAIL_PASSWORD = "gefnoodpvwmzwfgg"
+
+export const URL_ENCODING = true;
+export const MAX_JSON_SIZE = "50mb";
+
+export const REQUEST_LIMIT_TIME = 15 * 60 * 1000;
+export const REQUEST_LIMIT_NUMBER = 3000;
+
+export const WEB_CACHE = false;
+export const PORT = 3000;
+
+
